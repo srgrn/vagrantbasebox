827099f65cde84a521334288d319d2a373bda676
Eran Zimbler <eran@zimbler.net>
2013-09-29 04:48:42 +0000

    voteup and down

--- public/js/controllers/boxlist.js
+++ public/js/controllers/boxlist.js
@@ -2,8 +2,19 @@
 
 angular.module('vboxes')
   .controller('BoxlistCtrl', function ($scope,$location,$http) {
-  $http.get('/api/boxes').success(function(data) {
-		      $scope.boxes = data;
-  });
-  $location.path('/boxes')
+		$http.get('/api/boxes').success(function(data) {
+			$scope.boxes = data;
+		});
+		$scope.voteup = function (boxname){
+			$http.post('api/voteup',{ "name": boxname }).success(function(result){
+				console.log(result);
+			});
+		};
+		$scope.votedown = function (boxname){
+			$http.post('api/votedown',{ "name": boxname }).success(function(result){
+				console.log(result);
+			});
+		};
+		$location.path('/boxes');
+
   });
